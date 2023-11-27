e1cc7d46e5126e6f419db59a5b863c709a518e77
Senseless <100872213+SenselessDNS@users.noreply.github.com>
2023-11-27 19:38:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-os.system("ngrok config add-authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf || ./ngrok config add-authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf || ngrok authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf")
+os.system("ngrok config add-authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf && ./ngrok config add-authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf && ngrok authtoken 2EcFi3NgdSCjz541gmD1ChlnGOr_6wiepJMBeBSVCGoRkm5Lf")
 import hashlib
 from datetime import datetime
 from flask import Flask, request
